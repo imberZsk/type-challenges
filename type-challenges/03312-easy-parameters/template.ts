fc2e636717810a4649258e859abcfd1de7a5735a
imber <shikun.zhang@dreamsmart.com>
2025-07-16 16:06:51 +0800

    feat: 3312Parameters

--- type-challenges/03312-easy-parameters/template.ts
+++ type-challenges/03312-easy-parameters/template.ts
@@ -1,1 +1,9 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...any: infer S
+) => any
+  ? S
+  : any
+
+const foo = (arg1: string, arg2: number): void => {}
+
+type FunctionParamsType = MyParameters<typeof foo> // [arg1: string, arg2: number]
